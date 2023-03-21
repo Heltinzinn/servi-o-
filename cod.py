f1c8224217ea52d818e08b7245d5c3d3ad259ba3
unknown <tad-helitonlima@ucpparana.edu.br>
2023-03-20 21:50:00 -0300

    dksfksdfn

---
+++ cod.py
@@ -0,0 +1,14 @@
+# Armazenar informações pessoais em variáveis
+nome = "João"
+sobrenome = "Silva"
+idade = 25
+altura = 1.75
+peso = 70.5
+maior_de_idade = idade >= 18
+
+# Exibir resultados
+print("Nome completo:", nome, sobrenome)
+print("Idade:", idade, "anos")
+print("Altura:", altura, "metros")
+print("Peso:", peso, "quilos")
+print("Maior de idade:", maior_de_idade)
